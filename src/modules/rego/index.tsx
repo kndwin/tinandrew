d4dff44e02f8f8ec31f9e45a79bf09b141bd1751
kndwin <tnkevino@gmail.com>
2023-08-06 07:13:15 +0700

    fix: add trim to reduce spaces

--- src/modules/rego/index.tsx
+++ src/modules/rego/index.tsx
@@ -11,8 +11,8 @@ import { FormTextField, FormPasswordField, Text } from "~/ui";
 import { useRouter } from "next/router";
 
 export const validateRegoSchema = z.object({
-  firstName: z.string().min(1),
-  lastName: z.string().min(1),
+  firstName: z.string().trim().min(1),
+  lastName: z.string().trim().min(1),
   email: z.string().email(),
   password: z.string().min(8),
 });
